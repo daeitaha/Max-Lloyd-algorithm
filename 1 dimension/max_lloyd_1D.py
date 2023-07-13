0f2771b531b63862e5d3820ad5dbcb2c63599dbe
daeitaha <taha.asgari988@gmail.com>
2023-07-13 02:38:27 -0700

    update

--- 1 dimension/max_lloyd_1D.py
+++ 1 dimension/max_lloyd_1D.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 import numpy as np
 import time
-
+import torch
 
 
 # def uniform(t):
@@ -30,42 +30,61 @@ import time
 #     return random.uniform(-1,1)
 #     #return random.gauss(0,1)
 
-def trapezoid_fast_integral(f, thresholds, n_int=1000):
-    n_th = thresholds.size
-    grid = np.linspace(thresholds[np.arange(n_th-1)],thresholds[np.arange(n_th-1)+1],n_int)
-    delta =  grid[1,0]-grid[0,0]
-    f_grid = f(grid)
-    f_grid[1:-1,:] = 2*f_grid[1:-1,:]
-    integration = np.sum(f_grid*delta/2,axis=0)
+# def trapezoid_fast_integral(f, thresholds, n_int=1000):
+#     n_th = thresholds.size
+#     grid = np.linspace(thresholds[np.arange(n_th-1)],thresholds[np.arange(n_th-1)+1],n_int)
+#     delta =  grid[1,0]-grid[0,0]
+#     f_grid = f(grid)
+#     f_grid[1:-1,:] = 2*f_grid[1:-1,:]
+#     integration = np.sum(f_grid*delta/2,axis=0)
     
-    return integration
+#     return integration
     
 # f = lambda x: x**2
 # thresholds = np.arange(10)
 # y = trapezoid_fast_integral(f, thresholds, n_int=1000)
 
 # computes MSE between 2 adjacent decision thresholds (on one segment)
-def interval_MSE(x, t1, t2, f):
-    return integrate.quad(lambda t: ((t - x)**2) * f(t), t1, t2)[0]
+# def interval_MSE(x, t1, t2, f):
+#     return integrate.quad(lambda t: ((t - x)**2) * f(t), t1, t2)[0]
 
-# computes mean squared error on R
-def MSE(t, x, f):
-    # s = interval_MSE(x[0], -float('Inf'), t[0], f) + interval_MSE(x[-1], t[-1], float('Inf'), f)
-    s = interval_MSE(x[0], -np.pi, t[0], f) + interval_MSE(x[-1], t[-1], np.pi, f)
-    for i in range(1,len(x)-1):
-        s = s + interval_MSE(x[i], t[i-1], t[i], f)
-    return s
+# # computes mean squared error on R
+# def MSE(t, x, f):
+#     # s = interval_MSE(x[0], -float('Inf'), t[0], f) + interval_MSE(x[-1], t[-1], float('Inf'), f)
+#     s = interval_MSE(x[0], -np.pi, t[0], f) + interval_MSE(x[-1], t[-1], np.pi, f)
+#     for i in range(1,len(x)-1):
+#         s = s + interval_MSE(x[i], t[i-1], t[i], f)
+#     return s
 
-# t1 and t2 are the boundaries of the interval on which the centroid is calculated
-def centroid(t1, t2, f):
-    if integrate.quad(f, t1, t2)[0] == 0 or t1 == t2:
-        # return 0
-        return (t1+t2)/2
-    else:
-        return integrate.quad(lambda t:t*f(t), t1, t2)[0] / integrate.quad(f, t1, t2)[0]
+# # t1 and t2 are the boundaries of the interval on which the centroid is calculated
+# def centroid(t1, t2, f):
+#     if integrate.quad(f, t1, t2)[0] == 0 or t1 == t2:
+#         # return 0
+#         return (t1+t2)/2
+#     else:
+#         return integrate.quad(lambda t:t*f(t), t1, t2)[0] / integrate.quad(f, t1, t2)[0]
     
+def quantization_error_calc(f, x , thresholds, n_int=1000):
+    n_th           = thresholds.size
+    grid           = np.linspace(thresholds[np.arange(n_th-1)],thresholds[np.arange(n_th-1)+1],n_int)
+    delta          =  grid[1,:]-grid[0,:]
+    f_grid         = f(grid)
+    f_grid[1:-1,:] = 2*f_grid[1:-1,:]
+    error          = np.sum(f_grid*((grid-x)**2)*delta/2)
+    return error
 
-def fast_centroid_paralel(f, thresholds, n_int=1000):
+
+def quantization_error_calc_torch(f, x , thresholds, n_int=1000):
+    device         = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    n_th           = thresholds.size
+    grid           = torch.linspace(thresholds[np.arange(n_th-1)],thresholds[np.arange(n_th-1)+1],n_int, device=device)
+    delta          =  grid[1,:]-grid[0,:]
+    f_grid         = f(grid)
+    f_grid[1:-1,:] = 2*f_grid[1:-1,:]
+    error          = torch.sum(f_grid*((grid-x)**2)*delta/2) 
+    return error
+
+def centroid_calc(f, thresholds, n_int=1000):
     n_th           = thresholds.size
     grid           = np.linspace(thresholds[np.arange(n_th-1)],thresholds[np.arange(n_th-1)+1],n_int)
     delta          =  grid[1,:]-grid[0,:]
@@ -75,66 +94,84 @@ def fast_centroid_paralel(f, thresholds, n_int=1000):
     num_int     = np.sum(f_grid*grid*delta/2,axis=0)
     denum_int   = np.sum(f_grid*delta/2,axis=0)
     
-    output       = num_int/denum_int
-    idx1         = np.where(denum_int==0)[0]
-    idx2         = np.unique(np.concatenate((idx1, idx1+1)))
-    output[idx1] = moving_average(thresholds[idx2], n=2).squeeze()
+    x      = num_int/denum_int
+    idx    = np.where(denum_int==0)[0]
+    x[idx] = (thresholds[idx] + thresholds[idx+1])/2
     
-    return output
-
-def centroid_paralel(f, t):
-    y = f(t)
-    num_int     = np.diff(integrate.cumulative_trapezoid(y*t, t, initial=0))
-    denum_int   = np.diff(integrate.cumulative_trapezoid(y, t, initial=0))
-    output = num_int/denum_int
-    idx1 = np.where(denum_int==0)[0]
-    idx2 = np.unique(np.concatenate((idx1, idx1+1)))
-    output[idx1] = moving_average(t[idx2], n=2).squeeze()
+    error   = np.sum(f_grid*((grid-x)**2)*delta/2) 
     
-    return output
+    return x, error
 
-    # if integrate.quad(f, t1, t2)[0] == 0 or t1 == t2:
-    #     # return 0
-    #     return (t1+t2)/2
-    # else:
-    #     return integrate.quad(lambda t:t*f(t), t1, t2)[0] / integrate.quad(f, t1, t2)[0]
+
+def centroid_calc_torch(f, thresholds, n_int=1000):
+    device         = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    n_th           = thresholds.size
+    grid           = torch.linspace(thresholds[np.arange(n_th-1)],thresholds[np.arange(n_th-1)+1],n_int, device=device)
+    delta          =  grid[1,:]-grid[0,:]
+    f_grid         = f(grid)
+    f_grid[1:-1,:] = 2*f_grid[1:-1,:]
+    
+    num_int     = torch.sum(f_grid*grid*delta/2,axis=0)
+    denum_int   = torch.sum(f_grid*delta/2,axis=0)
+    
+    x      = num_int/denum_int
+    idx    = torch.where(denum_int==0)[0]
+    x[idx] = (thresholds[idx] + thresholds[idx+1])/2
+    error  = torch.sum(f_grid*((grid-x)**2)*delta/2) 
+    
+    return x, error
+
+# def centroid_paralel(f, t):
+#     y = f(t)
+#     num_int     = np.diff(integrate.cumulative_trapezoid(y*t, t, initial=0))
+#     denum_int   = np.diff(integrate.cumulative_trapezoid(y, t, initial=0))
+#     output = num_int/denum_int
+#     idx1 = np.where(denum_int==0)[0]
+#     idx2 = np.unique(np.concatenate((idx1, idx1+1)))
+#     output[idx1] = moving_average(t[idx2], n=2).squeeze()
+    
+#     return output
+
+#     # if integrate.quad(f, t1, t2)[0] == 0 or t1 == t2:
+#     #     # return 0
+#     #     return (t1+t2)/2
+#     # else:
+#     #     return integrate.quad(lambda t:t*f(t), t1, t2)[0] / integrate.quad(f, t1, t2)[0]
 
 # t is an array containing the initial decision thresholds
 # x is an array containing the representation levels
 # error_threshold is the threshold to reach for the algorithm to stop
-def maxlloyd(input_t, input_x, f, error_threshold):
-    x = 1*input_x
-    t = 1*input_t
-    e = MSE(t, x, f)
-    error = [e]
-    c = 0
-    while e > error_threshold and c < 10:
-        
-        c = c+1
-        t =  moving_average(x, n=2)
-        # if c%2 == 1:
-        #     t =  moving_average(x, n=2)
-        # else:
-        # # adjust levels
-        # tt = time.time()
-        # x[0] = centroid(-np.pi, t[0], f)
-        # x[-1] = centroid(t[-1], np.pi, f)
-        # for i in range(1,len(x)-1):
-        #     x[i] = centroid(t[i-1], t[i], f)
-        # print('original time:%.4f'%(time.time()-tt))
-        # x_tmp = x
-        
-        # tt = time.time()
-        x = fast_centroid_paralel(f, np.concatenate((np.array([-np.pi]),t.squeeze(),np.array([np.pi]))),n_int=1000)
-        # print('New time:%.4f'%(time.time()-tt))
-
-            
-        
-        e = MSE(t, x, f)
+def maxlloyd_torch(input_t, input_x, f, error_threshold, max_iter=10):
+    device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    x       = 1*input_x
+    t       = 1*input_t
+    e       = quantization_error_calc_torch(f, x , t, n_int=1000)
+    error   = [e]
+    c       = 0
+    while e > error_threshold and c < max_iter:
+        c       = c+1
+        t       =  moving_average_torch(x, n=2)
+        x, e    = centroid_calc_torch(f, torch.concatenate((torch.tensor([-torch.pi]),t.squeeze(),torch.tensor([torch.pi]))),n_int=1000)
         error.append(e)
         if ((error[-1]/error[-2])>0.8):
             break
-        # print(e)
+    return x, t, error
+
+
+def maxlloyd(input_t, input_x, f, error_threshold, max_iter=10):
+    x       = 1*input_x
+    t       = 1*input_t
+    e       = quantization_error_calc(f, x , t, n_int=1000)
+    error   = [e]
+    c       = 0
+    while e > error_threshold and c < max_iter:
+        
+        c    = c+1
+        t    =  moving_average(x, n=2)
+        x, e = centroid_calc(f, np.concatenate((np.array([-np.pi]),t.squeeze(),np.array([np.pi]))),n_int=1000)
+        error.append(e)
+        if ((error[-1]/error[-2])>0.8):
+            break
     return x, t, error
 
 def moving_average(a, n=3):
@@ -146,6 +183,18 @@ def moving_average(a, n=3):
         ret = np.cumsum(a, dtype=np.float64, axis=0)
     ret[n:,:] = ret[n:,:] - ret[:-n,:]
     return ret[n - 1:,:] / n
+
+
+def moving_average_torch(a, n=3):
+    if (len(a.shape)<2):
+        a = a.reshape(-1,1)
+    if (torch.is_complex(a)):
+        ret = torch.cumsum(a, dtype=torch.complex128, axis=0)
+    else:
+        ret = torch.cumsum(a, dtype=torch.float64, axis=0)
+    ret[n:,:] = ret[n:,:] - ret[:-n,:]
+    return ret[n - 1:,:] / n
+
 
 # k = 10
 # mu = 0
